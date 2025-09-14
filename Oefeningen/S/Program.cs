5b47bdfb9fba0dcc1fb6ca788f9d51de76e29041
Nick Dekker <DEKKERNI@rdw.nl>
2025-09-14 15:46:33 +0200

    breid presentatie uit

--- Oefeningen/S/Program.cs
+++ Oefeningen/S/Program.cs
@@ -4,6 +4,7 @@ using S.Models;
 /// In dit project gaat het om het Single responsibility principle (SRP).
 /// 
 /// OPDRACHT: Herschrijf de Medewerker-class zodanig dat deze aan dit principe voldoet.
+/// De code moet wel hetzelfde resultaat geven.
 
 var jan = new Medewerker("Jan", "Kampen", 5000);
 jan.BetaalLoon();
