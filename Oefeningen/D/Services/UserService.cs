c4afbf231c2ab7a7a3f74e0680b9117da71f6912
Nick Dekker <DEKKERNI@rdw.nl>
2025-09-18 16:39:45 +0200

    WIP

--- Oefeningen/D/Services/UserService.cs
+++ Oefeningen/D/Services/UserService.cs
@@ -1,9 +1,16 @@
-﻿public class UserService
+﻿using D.Database;
+
+public class UserService
 {
-    private readonly UserRepository _userRepository = new UserRepository();
+    private readonly FakeSqlDbImplementation _dataBase;
+
+    public UserService()
+    {
+        _dataBase = new FakeSqlDbImplementation();
+    }
 
     public void RegisterUser(string username)
     {
-        _userRepository.AddUser(username);
+        _dataBase.SaveUser(username);
     }
 }
